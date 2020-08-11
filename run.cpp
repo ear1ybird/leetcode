fc0b9e6a70eee31b99fadf849b29857939f9ae36
ZhaiGuiyu <ear1ybird@outlook.com>
2020-08-11 09:37:34 +0800

    first leetcode

---
+++ run.cpp
@@ -0,0 +1,14 @@
+#include "1.Two Sum.h"
+#include <iostream>
+
+int main()
+{
+    Solution solution;
+    vector<int> nums={4, 7, 2, 15};
+    vector<int> result = solution.twoSum(nums, 9);
+    
+    for(int i:result){
+        cout<<i<<endl;
+    }
+    return 0;
+}

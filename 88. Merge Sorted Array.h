f9572b88f37a2ac190590549210cd8367832aaf1
ZhaiGuiyu <ear1ybird@outlook.com>
2020-08-11 15:59:20 +0800

    文件说明

--- 88. Merge Sorted Array.h
+++ 88. Merge Sorted Array.h
@@ -1,3 +1,11 @@
+/*************************************************************************
+    Copyright:ear1ybird
+    Author:ear1ybird
+    Date:2020-08-11
+    Description:给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 
+    nums1 中，使 nums1 成为一个有序数组。
+**************************************************************************/
+
 #include <vector>
 #include <iostream>
 using namespace std;

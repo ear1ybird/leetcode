476c33ece14953db61e4d9d82f2e976728b9afe3
ZhaiGuiyu <ear1ybird@outlook.com>
2020-08-16 09:57:03 +0800

    refactor：09提升性能

--- 9. Palindrome Number.h
+++ 9. Palindrome Number.h
@@ -4,7 +4,7 @@
     Date:2020-08-16
     Description:判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从
     右向左）读都是一样的整数。
-    Agorithm:
+    Agorithm:整数反转一半
 **************************************************************************/
 
 #include <iostream>
@@ -20,31 +20,33 @@ public:
         if (x < 0)
         {
             return false;
-        }else if(x==0){
+        }
+        else if (x == 0)
+        {
             return true;
         }
 
-        vector<int> s;
-
-        while (x != 0)
+        if (x % 10 == 0)
         {
-            int pop = x % 10;
-            s.push_back(pop);
+            return false;
+        }
+
+        int revertedNum = 0;
+        while (x > revertedNum)
+        {
+            revertedNum *= 10;
+            revertedNum += x % 10;
+
             x /= 10;
         }
 
-        auto left = s.begin();
-        auto right = s.end() - 1;
-        while (left < right)
+        if (x == revertedNum || x == revertedNum / 10)
         {
-            if ((*left) != (*right))
-            {
-                return false;
-            }
-            left++;
-            right--;
+            return true;
         }
-
-        return true;
+        else
+        {
+            return false;
+        }
     }
 };
